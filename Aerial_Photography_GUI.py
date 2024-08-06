5f9530b9e80bf3ad59b77ba03d311038e2a30081
ji-tf <madarich18@gmail.com>
2024-08-06 14:29:25 +0400

    Test

--- Aerial_Photography_GUI.py
+++ Aerial_Photography_GUI.py
@@ -247,6 +247,14 @@ class AerialPhotographyGUI:
         file_path = self.dockwidget.button_choise_txt.filePath()
         file_widget = QgsFileWidget()
         file_widget.setFilePath(file_path)
+
+        if file_path:
+            filename, filter = QFileDialog.getOpenFileName(self.dockwidget, 'Выбрать текстовый файл', '', filter='Текстовые файлы (*.txt)')
+            file_widget.setFilePath(filename)
+
+        #file_path = self.dockwidget.button_choise_txt.filePath()
+        #file_widget = QgsFileWidget()
+        #file_widget.setFilePath(file_path)
         #filename, filter = QFileDialog.getOpenFileName(self.dockwidget, 'Выбрать текстовый файл', '', filter='Текстовые файлы (*.txt)')
         #file_widget.setFilePath(filename)
 
