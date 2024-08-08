2278800c2e32095b9cf660f69a1b0cec74ce27b0
ji-tf <madarich18@gmail.com>
2024-08-08 11:17:08 +0400

    Save fun

--- Aerial_Photography_GUI.py
+++ Aerial_Photography_GUI.py
@@ -28,7 +28,7 @@ from qgis.gui import QgsFileWidget
 from qgis.PyQt.QtWidgets import QFileDialog
 from qgis.utils import iface
 #from PyQt5.QtWidgets import QTextDocument
-from PyQt5.QtWidgets import QTextTable
+#from PyQt5.QtWidgets import QTextTable
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
