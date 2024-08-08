6739fc4ce1b2593838153f3915401b94b92538eb
ji-tf <madarich18@gmail.com>
2024-08-08 13:59:32 +0400

    Save fun

--- Aerial_Photography_GUI.py
+++ Aerial_Photography_GUI.py
@@ -27,8 +27,7 @@ from qgis.PyQt.QtWidgets import QAction
 from qgis.gui import QgsFileWidget
 #from qgis.PyQt.QtWidgets import QFileDialog
 #from qgis.utils import iface
-from qgis.PyQt.QtWidgets import QTextDocument
-from qgis.PyQt.QtWidgets import QTextTable
+from PyQt5.QtGui import QTextDocument,QTextCursor,QTextTableFormat,QPixmap,QTextLength,QPixmap,QTextBlockFormat,QTextCharFormat,QPen,QTextFrameFormat,QIcon
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
